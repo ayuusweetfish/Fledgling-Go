6ef33eeddd00b0b63dcfbc575f489efa6a452d22
Shiqing <shiqing-thu18@yandex.com>
2020-09-28 18:59:15 +0800

    Use compact sections and support C source

---
+++ kayubox/demo/bar.c
@@ -0,0 +1,5 @@
+int bar(int i)
+{
+  const int t[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  return t[i] / 3;
+}
--- kayubox/demo/foo.s
+++ kayubox/demo/foo.s
@@ -1,5 +1,12 @@
 .global foo
 foo:
-  mov   r0, #79
+  mov   r0, #3
+  bl    bar
+  add   r0, #79
   ldr   r1, =#435
+  ldr   r2, =year
   bx    lr
+
+.section .data
+year:
+  .word 2020
