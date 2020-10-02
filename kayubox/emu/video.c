2f32115f0783b08921fd7cf36141ed2badec9804
Shiqing <shiqing-thu18@yandex.com>
2020-10-02 17:23:47 +0800

    Textures test

--- kayubox/emu/video.c
+++ kayubox/emu/video.c
@@ -3,11 +3,18 @@
 #define GL_SILENCE_DEPRECATION
 #include "GLFW/glfw3.h"
 
+#if _WIN32
+#include <GL/glext.h>
+#endif
+
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 static GLFWwindow *window;
+
+static GLuint tex;
 
 void video_init()
 {
@@ -26,6 +33,38 @@ void video_init()
 
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  int w = 32, h = 16;
+  uint8_t *pix = (uint8_t *)malloc(w * h * 4);
+  for (int r = 0; r < h; r++)
+    for (int c = 0; c < w; c++) {
+      int R, G, B, A;
+      R = (uint8_t)((1 - (float)r / h) * 255.5f);
+      G = 0x7f;
+      B = (uint8_t)((1 - (float)c / w) * 255.5f);
+      A = 0xff;
+      if (r == 0 && c == 0) {
+        R = G = B = 0x3f;
+      } else if (((r ^ c) & 1) == 0) {
+        A = 0;
+      }
+      pix[(r * w + c) * 4 + 0] = R;
+      pix[(r * w + c) * 4 + 1] = G;
+      pix[(r * w + c) * 4 + 2] = B;
+      pix[(r * w + c) * 4 + 3] = A;
+    }
+
+  glGenTextures(1, &tex);
+  glBindTexture(GL_TEXTURE_2D, tex);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0,
+    GL_RGBA, GL_UNSIGNED_BYTE, pix);
 }
 
 bool video_running()
@@ -43,6 +82,7 @@ void video_test()
   glClearColor(0.95f, 0.9f, 0.9f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
 
+  glDisable(GL_TEXTURE_2D);
   glBegin(GL_TRIANGLES);
     glColor3f(1.0f, 0.7f, 0.3f);
     glVertex2f(0.0f, 0.0f);
@@ -52,6 +92,18 @@ void video_test()
     glVertex2f(0.0f, 0.0f);
     glVertex2f(0.25f - 0.08f * sin(glfwGetTime()), -0.5f);
     glVertex2f(0.5f, 0.0f);
+  glEnd();
+
+  glEnable(GL_TEXTURE_2D);
+  glBindTexture(GL_TEXTURE_2D, tex);
+  glBegin(GL_TRIANGLES);
+    glColor4f(1.0, 1.0, 1.0, 0.7);
+    for (int i = 0; i < 2; i++) {
+      glTexCoord2f(1.05, -0.05); glVertex2f(0.3, 0.3);
+      glTexCoord2f(-0.05, 1.05); glVertex2f(-0.9, -0.9);
+      glTexCoord2f(-0.05 + i * 1.1, -0.05 + i * 1.1);
+      glVertex2f(0.3 + (-1 + i) * 1.2f, 0.3 - i * 1.2f);
+    }
   glEnd();
 
   glfwSwapBuffers(window);
