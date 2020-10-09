2076a4f676a431b87b8ca0158f26bf8b81632366
Shiqing <shiqing-thu18@yandex.com>
2020-10-09 12:56:42 +0800

    Update submodules and add miniaudio dependency

---
+++ kayubox/emu/miniaudio_impl.c
@@ -0,0 +1,9 @@
+#define MA_NO_WAV
+#define MA_NO_FLAC
+#define MA_NO_MP3
+#define MA_NO_DECODING
+#define MA_NO_ENCODING
+#define MA_NO_GENERATION
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "ext/miniaudio/miniaudio.h"
