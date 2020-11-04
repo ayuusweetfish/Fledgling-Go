1925567e72474f7f844173e88e07e4f603f143aa
Starrah <46821571+starrah@users.noreply.github.com>
2020-11-04 22:08:53 +0800

    全局屏幕坐标转换与二次插值

---
+++ src/camera.s
@@ -0,0 +1,67 @@
+.include "common_macro.s"
+
+// 世界坐标系规定如下：
+// 世界坐标系想象为一个无限大的平面，向右为x轴正方向，向上为y轴正方向，远离屏幕为z轴正方向。
+// 在世界中，相机在x方向上以恒定速率(就是bpm)运动，在y轴上按照一定的运动算法作运动。
+// 当前的相机实现不考虑z轴，即对于不同深度的物体绘图时按照相同的比例投影到屏幕上，不考虑近大远小。
+//
+// 本游戏中所有的方形图片的position表示，全部以左上角为准。
+// 因为不考虑近大远小，当前的函数只提供了全局点坐标到屏幕点坐标的对应转换，暂未提供全局rect到屏幕rect的对应转换。
+//
+// camera绘制的范围固定为一个矩形，左上角的坐标始终是(当前拍号-2, camera_y)，长始终是12，高始终是9。
+// 游戏开始时，鸟的纵坐标为0，camera_y为4（对应于鸟正好在屏幕中间）。
+// camera_y是一个浮点数，表示的是游戏开始时
+
+// 有用的常量定义
+.equ  CAM_X_OFF,  2.0
+.equ  CAM_WID,    12.0
+.equ  CAM_HEI,    9.0
+
+// 衍生的常量定义
+// 这里的值是由上面的有用常量经过简单的计算得出的。如果修改上面的有用常量，应当按照公式对应修改这些内容
+.equ  _CWH,  6.0  // (CAM_WID / 2)
+.equ  _CXA,  -4.0 // (CAM_X_OFF - (CAM_WID / 2))
+.equ  _CHH,  4.5  // (CAM_HEI / 2)
+.equ  _CYA,  4.5 // (CAM_HEI / 2)
+
+.section .text
+coord_g2s_pt:
+  // s0, s1 全局坐标系的x y
+  // ret: s0,s1屏幕坐标系的xy
+  // 该函数会修改r0，s0~s1，s7的值
+  ldr       r0, =bttime
+  vldr      s7, [r0]
+  vsub.f32  s0, s7
+  vldrs     s7, _CXA
+  vadd.f32  s0, s7
+  vldrs     s7, _CWH
+  vdiv.f32  s0, s7 // x算完了
+  ldr       r0, =camera_y
+  vldr      s7, [r0]
+  vsub.f32  s1, s7
+  vldrs     s7, _CYA
+  vadd.f32  s1, s7
+  vldrs     s7, _CHH
+  vdiv.f32  s1, s7 // x算完了
+  bx        lr
+
+coord_g2s_rect:
+  // s0,s1,s2 全局坐标系（左上角点）的xyz坐标，s3, s4矩形的宽和高
+  // ret: s0,s1左上角xy， s4,s5左下角xy， s8,s9右上角xy，是对应于svc画图的输入格式的
+  // 与输入输出无关的寄存器不会改变
+  // TODO 之后如果有需要的话就做近大远小
+  bl        coord_g2s_pt
+  vldrs     s5, _CWH
+  vdiv.f32  s3, s5
+  vadd.f32  s8, s0, s3
+  vmov      s9, s1
+  vldrs     s5, _CHH
+  vdiv.f32  s4, s5
+  vadd.f32  s5, s1, s4
+  vmov      s4, s0
+  bx        lr
+
+
+.section .data
+camera_y: // 相机的
+  .float  4.0
---
+++ src/utils/erp.s
@@ -0,0 +1,38 @@
+# vim: set ft=asm:
+.include "common_macro.s"
+
+.section .data
+qerp:
+  // 二次插值：函数y=2x^2(x\in[0,0.5))；y=-2(x-1)^2+1(x\in[0.5,1])
+  // s0 下界 s1 上界 （允许s0>s1）
+  // s2 插值点（介于0至1之间） 若s2小于0则如同=0返回s0 若s2大于1则如同=1返回s1
+  // return s0
+  // 改变s3
+  vcmp.f32  s2, #0.0
+  bxle      lr
+  vldrs     s3, 1.0
+  vcmp.f32  s2, s3
+  vmovge    s0, s1
+  bxge      lr
+  vldrs     s3, 0.5
+  vcmp.f32  s2, s3
+  blgt      gt0p5
+lt0p5:
+  vmul.f32  s2, s2
+  vldrs     s3, 2.0
+  vmul.f32  s2, s3
+  bl        qerpfn
+gt0p5:
+  vldrs     s3, 2.0
+  vsub.f32  s4, s2, s3
+  vmul.f32  s2, s4
+  vldrs     s3, -2.0
+  vmul.f32  s2, s3
+  vldrs.f32 s3, 1.0
+  vsub.f32  s2, s3
+qerpfn:
+  // 此时s2是0至1之间的值
+  vsub.f32  s1, s0
+  vmul.f32  s1, s2
+  vadd.f32  s0, s1
+  bx        lr
