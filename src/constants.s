8fdabb9789d159242a43697e98f2d11749ae4c8b
kuriko <yangxr18@mails.tsinghua.edu.cn>
2020-11-05 17:24:39 +0800

    更改了game_state.s的部分逻辑和函数实现（未测试）

--- src/constants.s
+++ src/constants.s
@@ -1,8 +1,9 @@
 // st_pose 可能的动作
 .set POSE_NORMAL, 0
-.set POSE_PERFECT, 1
-.set POSE_GREAT, 2
+.set POSE_MOV_PERFECT, 1
+.set POSE_MOV_GREAT, 2            //仅用于上下按键
 .set POSE_BUMP, 3
 .set POSE_READY_UP, 4
 .set POSE_READY_DOWN, 5
-.set POSE_FLAP, 6
+.set POSE_FLAP_PERFECT, 6
+.set POSE_FLAP_GREAT, 7           //仅用于拍翅膀
